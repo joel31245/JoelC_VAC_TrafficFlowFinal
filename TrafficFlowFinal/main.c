22049bd78d122efc944b2fafc9de4d36d62640d9
Joel31245 <joel31245@gmail.com>
2017-08-03 08:55:06 -0500

    Added beginning of parallelization. Finished steady state flow. Lots to do.

--- TrafficFlowFinal/main.c
+++ TrafficFlowFinal/main.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <math.h>
 #include "structVehicle.c"
+#include "omp.h"
 
 /* Program Creates a single lane of traffic and evaluates
     what happens when their is an acceleration difference
@@ -27,6 +28,9 @@
       - dv(j)/dt = y[v(j-1)(t) - v(j)(t)]   where y = lambda, a reaction time coefficient. In this application reaction time will be some constant
       - dv(j)/dt = v(j-1)(t') - v(j)(t')    where t' = y*t
 
+      NOTE: MAX Decel is 23.87 ft/s2
+      TODO: Need to find out how to put in seperation distance. Set up change in initial velocity of initial vehicle.
+
 */
 
 // Amount of vehicles on road. Change to malloc for dynamic array
@@ -40,27 +44,38 @@
 int main()
 {
     struct Vehicle road[N];
-    FILE *f = fopen("TrafficFlow.csv");
+    FILE *f = fopen("TrafficFlow.csv","w");
     short i=0, j=0; // loop counters
     float lambda = 1;
 
-    // Block asks for user input. (How many cars, trucks?, position of trucks, speed change
-        // Setting up the initial velocity of the lead vehicle
+    omp_set_num_threads(4); // Only got 4 CPUs on my laptop
+
+    /// Block asks for user input. (How many cars, trucks?, position of trucks, speed change)
+        // Setting up the initial velocity of the lead vehicle.
     road[0].v = 30;
 
         // Setting up lambda for each vehicle. Defaults to 1
+    #pragma omp parallel for private(lambda)
     for( i=0; i<N; i++ ){
         road[i].y = lambda;
     }
 
 
-    // Block of code calculates the change in velocity of each vehicle per timestep
-        // dv/dj is 0 for this sections
-    for( i=1; i<N; i++ ){
-        //for( j=0; j<10; j+= ){
-            road[i].
+    /// Block of code calculates the change in velocity of each vehicle per timestep
+        // dv/dj is 0 for this sections. NOTE Unable to parallelize
+    float sizeT = (float)tEnd/dt;
+    for( i=0; i<sizeT; i++ ){
+        printf("t(%f):  ",i*dt); fprintf(f, "%f,",i*dt);
+        for( j=1; j<N; j++ ){
+            road[j].v = road[j-1].v;
+            fprintf(f, "%f,",road[j].v); printf("%f  ",road[j].v);
         }
+        fprintf(f, "\n"); printf("\n");
     }
 
-    // Block of code evaluates a crash?
+    /// Block of code evaluates a crash?
+
+    fclose(f);
+
+    return 0;
 }
